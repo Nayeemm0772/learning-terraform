d847cfe0bce387768fe8eea08adec177dd706964
NAYEEM <nayeem0772@gmail.com>
2022-11-12 16:11:50 +0530

    Added Jenkins File

--- 04-remote-state/ec2.tf
+++ 04-remote-state/ec2.tf
@@ -2,10 +2,8 @@ resource "aws_instance" "sample" {
   ami                    = "ami-00d48a21603b2119b"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-
-  tags = {
-    Name = upper(var.tagName)
-  }
 }
 
-variable "tagName" {}
+output "ec2_public_ip" {
+  value = aws_instance.sample.public_ip
+}
