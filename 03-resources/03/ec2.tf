40df4810374097422ee185c8eb43004e125d67e9
NAYEEM <nayeem0772@gmail.com>
2022-11-12 15:51:34 +0530

    Added Jenkins File

--- 03-resources/03/ec2.tf
+++ 03-resources/03/ec2.tf
@@ -3,3 +3,7 @@ resource "aws_instance" "sample" {
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 }
+
+output "ec2_public_ip" {
+  value = aws_instance.sample.public_ip
+}
