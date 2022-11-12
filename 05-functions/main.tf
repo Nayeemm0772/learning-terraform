bd0880490617bc8b7bdc700dbef0d36364ae8370
NAYEEM <nayeem0772@gmail.com>
2022-11-12 16:31:23 +0530

    Added Jenkins File

--- 05-functions/main.tf
+++ 05-functions/main.tf
@@ -1,21 +1,7 @@
-
-
-variable "sample" {
-  default = ["abc", 123, "annamacharya"]
-}
-
-
 output "sample" {
-  value = element(var.sample, 2)
-}
-
-output "sample2" {
-  value = element(var.sample, 1)
-}
-
-
-output "sample3" {
   value = element(var.sample, 0)
 }
 
-
+variable "sample" {
+  default = ["abc", 123]
+}
