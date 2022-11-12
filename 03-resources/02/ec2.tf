2214ec22721d6b662578113649562c1058c3c945
NAYEEM <nayeem0772@gmail.com>
2022-11-12 15:22:22 +0530

    Added Jenkins File

--- 03-resources/02/ec2.tf
+++ 03-resources/02/ec2.tf
@@ -11,6 +11,6 @@ resource "aws_instance" "sample" {
 #  }
 }
 
-#output "ec2_public_ip" {
-#  value = aws_instance.sample.public_ip
-#}
+output "ec2_public_ip" {
+  value = aws_instance.sample.public_ip
+}
