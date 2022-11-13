7ab7c6947b6f3b62f6efcecafea28203872362dc
NAYEEM <nayeem0772@gmail.com>
2022-11-13 18:54:19 +0530

    Added TF File

--- 08-loops/03/main.tf
+++ 08-loops/03/main.tf
@@ -1,27 +1,25 @@
-#resource "aws_instance" "sample" {
-#  for_each      = var.instances
-#  ami           = "ami-00d48a21603b2119b"
-#  instance_type = element(var.instances, count.index)
-#  tags = {
-#    Name = elemet(var.instances, count.index)
-#  }
-#}
+resource "aws_instance" "sample" {
+  for_each      = var.instances
+  ami           = "ami-00d48a21603b2119b"
+  instance_type = each.value.instance_type
+  tags = {
+    Name = each.value.tagName
+  }
+}
 
 
 variable "instances" {
   default = {
     catalogue = {
       instance_type = "t3.micro"
-      tagName       = "frontend"
+      tagName       = "CATALOGUE"
     }
     cart = {
       instance_type = "t3.micro"
-      tagName       = "backend"
+      tagName       = "CART"
     }
 
   }
 }
 
-output "count" {
-  value = length(var.instances, 0)
-}
+
